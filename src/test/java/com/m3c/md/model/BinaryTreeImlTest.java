875d9911cc545dffe7ae5ab5d5fc879e8b8bdbf0
md3g11@soton.ac.uk <MDfocus1tab>
2018-04-04 08:48:32 +0100

    - BinaryTree init

---
+++ src/test/java/com/m3c/md/model/BinaryTreeImlTest.java
@@ -0,0 +1,44 @@
+package com.m3c.md.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinaryTreeImlTest {
+
+    @Test
+    public void getRootElement() {
+    }
+
+    @Test
+    public void getNumberOfElements() {
+    }
+
+    @Test
+    public void addElement() {
+    }
+
+    @Test
+    public void addElements() {
+    }
+
+    @Test
+    public void findElement() {
+    }
+
+    @Test
+    public void getLeftChild() {
+    }
+
+    @Test
+    public void getRightChild() {
+    }
+
+    @Test
+    public void getSortedTreeAsc() {
+    }
+
+    @Test
+    public void getSortedTreeDesc() {
+    }
+}
